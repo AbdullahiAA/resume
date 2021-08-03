119b6515ae9b268a71345d6928721bfb898e066e
Abdullah <osuolaleabdullahi@gmail.com>
2021-08-04 00:19:51 +0100

    Resume Updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ const Header = ({ data }) => {
     var name = data.name;
     var occupation = data.occupation;
     var description = data.description;
-    var city = data.address.city;
+    var zip = data.address.zip;
     var networks = data.social.map(function (network) {
       return (
         <li key={network.name}>
@@ -68,7 +68,7 @@ const Header = ({ data }) => {
             <TypeWriter typing={0.5}>{name ? `I'm ${name}.` : null}</TypeWriter>
           </h1>
           <h3>
-            Based in {city}. <span>{occupation}</span>. {description}.
+            Based in {zip}. <span>{occupation}</span>. {description}.
           </h3>
           <hr />
           <ul className="social">{networks}</ul>
